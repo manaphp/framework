b1f969f7baae5ac01536db8fe053a55faa62eaca
ManaPHP Mark <manaphp@qq.com>
2024-12-18 21:17:42 +0800

    \ManaPHP\Persistence\AutoFiller

--- Persistence/AutoFiller.php
+++ Persistence/AutoFiller.php
@@ -33,6 +33,36 @@ class AutoFiller implements AutoFillerInterface
         return null;
     }
 
+    protected function findCreatedTime(Entity $entity): ?string
+    {
+        return $this->findField($entity, $this->created_time);
+    }
+
+    protected function findCreatorId(Entity $entity): ?string
+    {
+        return $this->findField($entity, $this->creator_id);
+    }
+
+    protected function findCreatorName(Entity $entity): ?string
+    {
+        return $this->findField($entity, $this->creator_name);
+    }
+
+    protected function findUpdatedTime(Entity $entity): ?string
+    {
+        return $this->findField($entity, $this->updated_time);
+    }
+
+    protected function findUpdatorId(Entity $entity): ?string
+    {
+        return $this->findField($entity, $this->updator_id);
+    }
+
+    protected function findUpdatorName(Entity $entity): ?string
+    {
+        return $this->findField($entity, $this->updator_name);
+    }
+
     public function setTime(Entity $entity, string $field, int $timestamp): void
     {
         $rProperty = new ReflectionProperty($entity, $field);
@@ -48,37 +78,32 @@ class AutoFiller implements AutoFillerInterface
         $user_id = $this->identity->isGuest() ? 0 : $this->identity->getId();
         $user_name = $this->identity->isGuest() ? '' : $this->identity->getName();
 
-        $created_time = $this->findField($entity, $this->created_time);
+        $created_time = $this->findCreatedTime($entity);
         if ($created_time !== null && !isset($entity->$created_time)) {
             $this->setTime($entity, $created_time, $timestamp);
         }
 
-        $creator_id = $this->findField($entity, $this->creator_id);
+        $creator_id = $this->findCreatorId($entity);
         if ($creator_id !== null && !isset($entity->$creator_id)) {
             $entity->$creator_id = $user_id;
         }
 
-        $creator_name = $this->findField($entity, $this->creator_name);
+        $creator_name = $this->findCreatorName($entity);
         if ($creator_name !== null && !isset($entity->$creator_name)) {
             $entity->$creator_name = $user_name;
         }
 
-        $updated_time = $this->findField($entity, $this->updated_time);
+        $updated_time = $this->findUpdatedTime($entity);
         if ($updated_time !== null && !isset($entity->$updated_time)) {
             $this->setTime($entity, $updated_time, $timestamp);
         }
 
-        $updated_time = $this->findField($entity, $this->updated_time);
-        if ($updated_time !== null && !isset($entity->$updated_time)) {
-            $this->setTime($entity, $updated_time, $timestamp);
-        }
-
-        $updator_id = $this->findField($entity, $this->updator_id);
+        $updator_id = $this->findUpdatorId($entity);
         if ($updator_id !== null && !isset($entity->$updator_id)) {
             $entity->$updator_id = $user_id;
         }
 
-        $updator_name = $this->findField($entity, $this->updator_name);
+        $updator_name = $this->findUpdatorName($entity);
         if ($updator_name !== null && !isset($entity->$updator_name)) {
             $entity->$updator_name = $user_name;
         }
@@ -90,17 +115,17 @@ class AutoFiller implements AutoFillerInterface
         $user_id = $this->identity->isGuest() ? 0 : $this->identity->getId();
         $user_name = $this->identity->isGuest() ? '' : $this->identity->getName();
 
-        $updated_time = $this->findField($entity, $this->updated_time);
+        $updated_time = $this->findUpdatedTime($entity);
         if ($updated_time !== null) {
             $this->setTime($entity, $updated_time, $timestamp);
         }
 
-        $updator_id = $this->findField($entity, $this->updator_id);
+        $updator_id = $this->findUpdatorId($entity);
         if ($updator_id !== null) {
             $entity->$updator_id = $user_id;
         }
 
-        $updator_name = $this->findField($entity, $this->updator_name);
+        $updator_name = $this->findUpdatorName($entity);
         if ($updator_name !== null) {
             $entity->$updator_name = $user_name;
         }
