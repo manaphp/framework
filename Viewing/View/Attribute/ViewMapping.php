733974ceb75aec7cf3a3e4c7a6ed53aa3f1e5a96
ManaPHP Mark <manaphp@qq.com>
2024-12-22 18:17:21 +0800

    \ManaPHP\Viewing\View\Attribute\ViewMapping

--- Viewing/View/Attribute/ViewMapping.php
+++ Viewing/View/Attribute/ViewMapping.php
@@ -4,20 +4,11 @@ declare(strict_types=1);
 namespace ManaPHP\Viewing\View\Attribute;
 
 use Attribute;
+use ManaPHP\Http\Router\Attribute\Mapping;
 
 #[Attribute(Attribute::TARGET_METHOD)]
-class ViewMapping implements ViewMappingInterface
+class ViewMapping extends Mapping implements ViewMappingInterface
 {
-    public function __construct(public string|array|null $path = null)
-    {
-
-    }
-
-    public function getPath(): string|array|null
-    {
-        return $this->path;
-    }
-
     public function getMethod(): string
     {
         return 'GET';
