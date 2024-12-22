4475ba74777447416be0f84ac8236ef423ca50e2
ManaPHP Mark <manaphp@qq.com>
2024-12-22 16:57:20 +0800

    \ManaPHP\Mvc\View\Attribute\ViewMappingInterface

--- Mvc/View/Attribute/ViewMappingInterface.php
+++ Mvc/View/Attribute/ViewMappingInterface.php
@@ -3,10 +3,8 @@ declare(strict_types=1);
 
 namespace ManaPHP\Mvc\View\Attribute;
 
-use Attribute;
 use ManaPHP\Http\Router\Attribute\MappingInterface;
 
-#[Attribute(Attribute::TARGET_METHOD)]
 interface ViewMappingInterface extends MappingInterface
 {
 
