bb258848dab7a2ca035511dd4b27d37a1f2ff18f
ManaPHP Mark <manaphp@qq.com>
2024-12-31 19:47:47 +0800

    \ManaPHP\Http\Interceptor

--- Http/Interceptor.php
+++ Http/Interceptor.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace ManaPHP\Http;
 
+use ManaPHP\Di\Attribute\Autowired;
 use ReflectionMethod;
 
 class Interceptor implements InterceptorInterface
 {
+    #[Autowired] protected RequestInterface $request;
+    #[Autowired] protected ResponseInterface $response;
+
     public function preHandle(ReflectionMethod $method): bool
     {
         return true;
